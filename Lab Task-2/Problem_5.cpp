41a6d6b42d82b3ff9be8ba27fb614585b5017938
unknown <muzahidulislam.munna75@gmail.com>
2023-01-17 09:28:43 +0600

    lab task 2

---
+++ Lab Task-2/Problem_5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    float F,C;
+    cout<<"Enter fahrenheit temp :";
+    cin>>F;
+    C=(F-32)*5/9;
+    cout<<C;
+    getch();
+}
